41d62fec3572b299f12942916e1e66cd063d0c7b
Nwtype <newcratie@gmail.com>
2019-02-10 12:57:20 +0700

    Mess fixed. supasate/connected-react-router(kind of react-router-redux) implemented

--- client/src/react/store/middleware-store.js
+++ client/src/react/store/middleware-store.js
@@ -1,8 +1,10 @@
 import thunk from 'redux-thunk';
 import {applyMiddleware, compose} from 'redux';
 import logger from 'redux-logger';
+import { routerMiddleware } from 'connected-react-router';
+import {history} from "../modules/history-router.react";
 
-const middleware = [thunk, logger];
+const middleware = [thunk, logger,routerMiddleware(history)];
 const middlewares = compose(
     applyMiddleware(...middleware),
     window.devToolsExtension ? window.devToolsExtension() : f => f
