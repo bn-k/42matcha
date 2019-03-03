9788e8d76517a229941c18c9395e86ff178d3602
Nwtype <newcratie@gmail.com>
2019-03-03 12:00:11 +0100

    Postgres+pgadmin+api-go working toghether

--- server/main.go
+++ server/main.go
@@ -3,11 +3,11 @@ package main
 import (
 	"fmt"
 	"time"
-	_ "github.com/Newcratie/matcha-api"
+	"github.com/Newcratie/matcha-api"
 )
 
 func main() {
 	fmt.Println("API launched!!")
-	//api.Run()
+	api.Run()
 	time.Sleep(time.Hour * 4)
 }
