fcfdad444830ce92565ffc284a1d9f87fc5d20d2
Nwtype <newcratie@gmail.com>
2019-05-29 20:37:45 +0200

    add install.sh

--- back/install.sh
+++ back/install.sh
@@ -3,5 +3,5 @@ go get github.com/dgrijalva/jwt-go;
 go get github.com/gin-gonic/gin;
 go get gopkg.in/olahol/melody.v1;
 go get github.com/gin-contrib/cors;
-go get github.com/oxequa/realize
+go get github.com/oxequa/realize;
 go get github.com/gin-gonic/gin;
